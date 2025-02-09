2512903907be810edd494ad4565e61cf8a30c7c6
gamzamandu <galbimandu1126@gmail.com>
2025-02-09 22:57:49 +0900

    chore : 잡다한 css 개조

--- src/shared/components/CKEditer/WriteHeader/style.ts
+++ src/shared/components/CKEditer/WriteHeader/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const WriteHeader = styled.header`
-  padding: 0.75rem 0;
+  padding: 0.75rem 0.1rem 0 0.1rem;
   border-bottom: 1px solid #e9ecef;
 
   .title {
