b89427d9afa8143f61e663a13ccb9aa9bf420d31
obtuse-triangle <me@obtuse.kr>
2025-02-28 01:34:50 +0900

    chore: local.jhnara.asuscomm.com 도메인 허용

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import * as path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+  server: {
+    allowedHosts: ["local.jhnara.asuscomm.com"],
+  },
+});
