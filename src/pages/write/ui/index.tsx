06e94ef26f9d8ca5fb6a31235eb877d43cca1db4
galbimandu <galbimandu1126@gmail.com>
2025-03-02 00:00:31 +0900

    add : write router

--- src/pages/write/ui/index.tsx
+++ src/pages/write/ui/index.tsx
@@ -1,6 +1,10 @@
 import CKEditer from '@/shared/components/CKEditer/index.tsx';
+import { useParams } from 'react-router-dom';
 
 const Write = () => {
+  const { hashed_id } = useParams<{ hashed_id: string }>();
+  console.log(hashed_id);
+  
   return (
     <>
       <CKEditer />
