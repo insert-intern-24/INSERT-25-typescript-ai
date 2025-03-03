4809aeaf8c5a369f3d37614d283f32bf45d817d2
galbimandu <galbimandu1126@gmail.com>
2025-03-04 00:18:11 +0900

    chore : editor컴포넌트 네이밍 변경 add : write에서 기존 데이터 바인딩하도록 함

--- src/pages/write/ui/index.tsx
+++ src/pages/write/ui/index.tsx
@@ -1,13 +1,14 @@
-import CKEditer from '@/shared/components/CKEditer/index.tsx';
+import CKEditorComponent from '@/shared/components/CKEditor/index.tsx';
 import { useParams } from 'react-router-dom';
 
 const Write = () => {
-  const { hashed_id } = useParams<{ hashed_id: string }>();
-  console.log(hashed_id);
+  // const { hashed_id } = useParams<{ hashed_id: string }>();
+  // console.log(hashed_id);
   
+
   return (
     <>
-      <CKEditer />
+      <CKEditorComponent />
     </>
   )
 }
---
+++ src/shared/components/CKEditor/index.tsx
@@ -0,0 +1,386 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+import { useParams } from 'react-router-dom';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  DecoupledEditor,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  Base64UploadAdapter,
+  BlockQuote,
+  Bookmark,
+  BalloonToolbar,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  FullPage,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  HtmlComment,
+  HtmlEmbed,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  MediaEmbed,
+  Mention,
+  PageBreak,
+  Paragraph,
+  PasteFromMarkdownExperimental,
+  PasteFromOffice,
+  RemoveFormat,
+  ShowBlocks,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextPartLanguage,
+  TextTransformation,
+  TodoList,
+  Underline,
+  WordCount,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+
+import "./style.css";
+import * as S from "./WriteHeader/style";
+
+// const LICENSE_KEY = import.meta.env.VITE_CKEDITOR_LICENSE_KEY;
+const LICENSE_KEY = "GPL";
+
+export default function CKEditorComponent() {
+  const editorContainerRef = useRef(null);
+  const editorMenuBarRef = useRef(null);
+  const editorToolbarRef = useRef(null);
+  const editorRef = useRef(null);
+  const editorWordCountRef = useRef(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+  const { hashed_id } = useParams();
+
+  interface editorOriginDataType {
+    title: string;
+    content: string;
+    hashed_id: string;
+    updated_at: string;
+  }
+
+  const [editorOriginData, setEditorOriginData] = useState<editorOriginDataType>({
+    title: "",
+    content: "",
+    hashed_id: "",
+    updated_at: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/files/${hashed_id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include"
+        });
+        const data = await response.json();
+        setEditorOriginData(data);
+        console.log(data);
+        // CKEditor 준비 완료
+        setIsLayoutReady(true);
+        return () => setIsLayoutReady(false);      
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    })();
+  }, []);
+
+  const { editorConfig } = useMemo(() => {
+    if (!isLayoutReady) {
+      return {};
+    }
+
+    return {
+      editorConfig: {
+        toolbar: {
+          items: [
+            "heading",
+            "|",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "|",
+            "link",
+            "insertTable",
+            "|",
+            "alignment",
+            "|",
+            "outdent",
+            "indent",
+          ],
+          shouldNotGroupWhenFull: false,
+        },
+        plugins: [
+          Alignment,
+          Autoformat,
+          AutoImage,
+          AutoLink,
+          Autosave,
+          Base64UploadAdapter,
+          BlockQuote,
+          Bold,
+          Bookmark,
+          Code,
+          CodeBlock,
+          BalloonToolbar,
+          Essentials,
+          FindAndReplace,
+          FontBackgroundColor,
+          FontColor,
+          FontFamily,
+          FontSize,
+          FullPage,
+          GeneralHtmlSupport,
+          Heading,
+          Highlight,
+          HorizontalLine,
+          HtmlComment,
+          HtmlEmbed,
+          ImageBlock,
+          ImageCaption,
+          ImageInline,
+          ImageInsert,
+          ImageInsertViaUrl,
+          ImageResize,
+          ImageStyle,
+          ImageTextAlternative,
+          ImageToolbar,
+          ImageUpload,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          LinkImage,
+          List,
+          ListProperties,
+          MediaEmbed,
+          Mention,
+          PageBreak,
+          Paragraph,
+          PasteFromMarkdownExperimental,
+          PasteFromOffice,
+          RemoveFormat,
+          ShowBlocks,
+          SourceEditing,
+          SpecialCharacters,
+          SpecialCharactersArrows,
+          SpecialCharactersCurrency,
+          SpecialCharactersEssentials,
+          SpecialCharactersLatin,
+          SpecialCharactersMathematical,
+          SpecialCharactersText,
+          Strikethrough,
+          Style,
+          Subscript,
+          Superscript,
+          Table,
+          TableCaption,
+          TableCellProperties,
+          TableColumnResize,
+          TableProperties,
+          TableToolbar,
+          TextPartLanguage,
+          TextTransformation,
+          TodoList,
+          Underline,
+          WordCount,
+        ],
+        balloonToolbar: ["bold", "italic", "|", "link"],
+        fontFamily: {
+          supportAllValues: true,
+        },
+        fontSize: {
+          options: [10, 12, 14, "default", 18, 20, 22],
+          supportAllValues: true,
+        },
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+            {
+              model: "heading4",
+              view: "h4",
+              title: "Heading 4",
+              class: "ck-heading_heading4",
+            },
+            {
+              model: "heading5",
+              view: "h5",
+              title: "Heading 5",
+              class: "ck-heading_heading5",
+            },
+            {
+              model: "heading6",
+              view: "h6",
+              title: "Heading 6",
+              class: "ck-heading_heading6",
+            },
+          ],
+        },
+        htmlSupport: {
+          allow: [
+            {
+              name: /^.*$/,
+              styles: true,
+              attributes: true,
+              classes: true,
+            },
+          ],
+        },
+        initialData: `${editorOriginData.content}`,
+        licenseKey: LICENSE_KEY,
+        link: {
+          addTargetToExternalLinks: true,
+          defaultProtocol: "https://",
+          decorators: {
+            toggleDownloadable: {
+              mode: "manual",
+              label: "Downloadable",
+              attributes: {
+                download: "file",
+              },
+            },
+          },
+        },
+        menuBar: {
+          isVisible: true,
+        },
+        placeholder: "Type or paste your content here!",
+        table: {
+          contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableProperties",
+            "tableCellProperties",
+          ],
+        },
+      },
+    };
+  }, [isLayoutReady]);
+
+    return (
+      <div className="main-container">
+        <div
+          className="editor-container editor-container_document-editor editor-container_include-word-count"
+          ref={editorContainerRef}
+        >
+          <S.WriteHeader>
+            <p className="title">title</p>
+            <div className="editor-container__menu-bar" ref={editorMenuBarRef}></div>
+          </S.WriteHeader>
+          <div className="editor-container__toolbar" ref={editorToolbarRef}></div>
+          <div className="editor-container__editor-wrapper">
+            <div className="editor-container__editor">
+              <div ref={editorRef}>
+                {editorConfig && (
+                  <CKEditor
+                    onReady={(editor) => {
+                      const wordCount = editor.plugins.get("WordCount");
+                      editor.model.document.on('change:data', () => {
+                        const data = editor.getData();
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(data, 'text/html');
+                        const bodyContent = doc.body ? doc.body.innerHTML : '';
+                        // CKditor 5의 content 추출
+                        console.log('Editor content changed:', bodyContent);
+                      });
+                      editorWordCountRef.current.appendChild(wordCount.wordCountContainer);
+                      editorToolbarRef.current.appendChild(editor.ui.view.toolbar.element);
+                      editorMenuBarRef.current.appendChild(editor.ui.view.menuBarView.element);
+                    }}
+                    onAfterDestroy={() => {
+                      Array.from(editorWordCountRef.current.children).forEach((child) =>
+                        child.remove()
+                      );
+                      Array.from(editorToolbarRef.current.children).forEach((child) =>
+                        child.remove()
+                      );
+                      Array.from(editorMenuBarRef.current.children).forEach((child) =>
+                        child.remove()
+                      );
+                  }}
+                  editor={DecoupledEditor}
+                  config={editorConfig}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="editor_container__word-count" ref={editorWordCountRef}></div>
+      </div>
+    </div>
+  );
+}
