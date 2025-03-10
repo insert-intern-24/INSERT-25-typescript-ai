865a31757c8ae6d092f68c634cbbc03a92d59d35
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-10 11:45:12 +0900

    feat: props로 보내기 위한 객체 생성

--- src/component/write/sideBar/SideBar.tsx
+++ src/component/write/sideBar/SideBar.tsx
@@ -1,25 +1,41 @@
 import styled from "styled-components";
 import TabsBlock from "./tabs/TabsBlock";
-import ListItem from "./list/ListItem";
+import ListItem from "./list/ErrorListItem";
 import OpenListItem from "./list/OpenListItem";
 import RefinedItem from "./list/RefinedItem";
 
+const word = [
+  {
+    default: "핸들링",
+    refine: "처리",
+    description: "외래어 직역 표현",
+  },
+  {
+    default: "노잼",
+    refine: "재미가 없는, 흥미가 퇴색된",
+    description: "불필요한 줄임말 외래어 사용",
+  },
+];
 export default function SideBar() {
   return (
     <>
       <SideBarBox>
         <TabsBlock />
-        <ListItem />
-        <OpenListItem />
-        <RefinedItem />
+        <ListItem default={word[1].default} description={word[1].description} />
+        <OpenListItem
+          default={word[0].default}
+          refine={word[0].refine}
+          description={word[0].description}
+        />
+        <RefinedItem default={word[1].default} refine={word[1].refine} />
       </SideBarBox>
     </>
   );
 }
 
 const SideBarBox = styled.div`
-  min-width: 500px;
-  min-height: 1003px;
+  max-width: 500px;
+  max-height: 1003px;
   flex-shrink: 0;
   border-left: 1px solid #e2e2e2;
   background: #fff;
