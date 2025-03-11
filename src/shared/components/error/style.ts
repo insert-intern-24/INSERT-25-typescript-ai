328e81bf34d406c2c3d873d66653e5ca8666f7f5
galbimandu <galbimandu1126@gmail.com>
2025-03-12 03:40:19 +0900

    add : error 하이라이트용 클래스 생성

--- src/shared/components/error/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const errorColor = "#FF5E5E";
-
-export const Error = styled.span`
-    background: rgba(255, 94, 94, 0.24);
-    border-bottom: 2px solid ${errorColor};
-`;
